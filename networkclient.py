248b70c736a90dc72a32d958f8d85d283d4645f8
Polymero <sebastiaanvenendaal@gmail.com>
2023-08-30 01:15:35 +0200

    202308300115

--- networkclient.py
+++ networkclient.py
@@ -4,10 +4,22 @@
 #
 
 # Imports
-from socket import *
+import socket
 import os, sys
 import random
 import json, pickle
+
+
+def getIP():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.settimeout(0)
+	try:
+		s.connect(('10.254.254.254', 1))
+		ip = s.getsockname()[0]
+	except:
+		ip = '127.0.0.1'
+	s.close()
+	return ip
 
 
 if len(sys.argv) < 2:
@@ -19,13 +31,13 @@ if sys.argv[1] in ['-h', '--host']:
 
 	HOST = 1
 
-	recv_socket = socket(AF_INET, SOCK_STREAM)
+	recv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	try: recv_socket.bind(('', int(sys.argv[2])))
 	except: recv_socket.bind(('', 12000))
 
 	recv_socket.listen()
-	print('Hosting on PORT', recv_socket.getsockname()[1])
+	print('Hosting on', getIP(), recv_socket.getsockname()[1])
 	print('Waiting for other player to connect...')
 
 	connection, address = recv_socket.accept()
@@ -37,7 +49,7 @@ if sys.argv[1] in ['-j', '--join']:
 
 	HOST = 0
 
-	connection = socket(AF_INET, SOCK_STREAM)
+	connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	try: connection.connect((sys.argv[2], int(sys.argv[3])))
 	except: print('Failed to join game...'); exit()
@@ -45,37 +57,96 @@ if sys.argv[1] in ['-j', '--join']:
 	print('Joined game at', connection.getpeername())
 
 
-class Game:
+#--------------------
+# GAME LOOP
+#--------------------
+
+BOARD = """
+ {} | {} | {}
+---+---+---
+ {} | {} | {} 
+---+---+---
+ {} | {} | {}
+"""
+
+class GAME:
 	def __init__(self):
-		self.hand = list(range(24))
-		random.shuffle(self.hand)
-		
+		self.state = 9 * [' ']
 
 	def __getstate__(self):
 		return self.__dict__
+	
+	def __setstate__(self, objdict):
+		self.__dict__ = objdict
 
-	def __setstate__(self, game_dict):
-		self.__dict__ = game_dict
+game = GAME()
 
+if HOST:
+	PLAYER = 'X'
+else:
+	PLAYER = 'O'
+
+print('Y:')
+print(BOARD.format(game.state))
 
 if HOST:
 
-	game = Game()
-
+	move = input('Move? (1-9) ')
+	game.state[int(move)] = PLAYER
 	connection.send(pickle.dumps(game))
 
+else:
 
-for _ in range(10):
+	print('Waiting for host to make move...')
+
+while True:
 
 	game = pickle.loads(connection.recv(1024))
+	print(BOARD.format(game.state))
 
-	print('<', game.hand)
-
-	random.shuffle(game.hand)
-
-	print('>', game.hand)
-
+	move = input('Move? (1-9) ')
+	game.state[int(move)] = PLAYER
 	connection.send(pickle.dumps(game))
+
+
+
+
+
+
+
+
+
+# class Game:
+# 	def __init__(self):
+# 		self.hand = list(range(24))
+# 		random.shuffle(self.hand)
+		
+
+# 	def __getstate__(self):
+# 		return self.__dict__
+
+# 	def __setstate__(self, game_dict):
+# 		self.__dict__ = game_dict
+
+
+# if HOST:
+
+# 	game = Game()
+
+# 	connection.send(pickle.dumps(game))
+
+
+# for _ in range(10):
+
+# 	game = pickle.loads(connection.recv(1024))
+
+# 	print('<', game.hand)
+
+# 	random.shuffle(game.hand)
+
+# 	print('>', game.hand)
+
+# 	connection.send(pickle.dumps(game))
 
 
 
